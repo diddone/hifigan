9119d3b8b3a64c43ea1a8401575a97087775264b
diddone <dmpozdeev@edu.hse.ru>
2022-12-21 16:59:16 +0300

    G

--- models/loss.py
+++ models/loss.py
@@ -5,18 +5,15 @@ class FeatureLoss(nn.Module):
     def __init__(self):
         super().__init__()
 
-        self.l1_loss = nn.L1Loss(reduction='none')
-
     def forward(self, inputs_list, targets_list):
         loss = 0.
 
         for inputs, targets in zip(inputs_list, targets_list):
             for x, y in zip(inputs, targets):
-                batch_size = x.size(0)
-                n_features = x.numel() / batch_size
+                l1_loss = nn.L1Loss(reduction='none')
                 coef = 1. / x.numel()
                 print(x.shape, y.shape, coef)
-                loss += coef * self.l1_loss(x, y)
+                loss += coef * l1_loss(x, y)
 
         return loss
 
